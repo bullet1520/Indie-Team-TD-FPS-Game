78b1ed12c574495a2cd80f1dfa797e7bd132d8bc
Eric Andruszko <bullet1520@users.noreply.github.com>
2018-09-25 15:05:44 -0500

    Win/Lose and First Level
    Began adding elements for Win and Lose screens
    Began adding terrain for proper demo level

---
+++ Assets/Scripts/WinLoseScript.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinLoseScript : MonoBehaviour
+{
+    public GameObject LoseScreen;
+    public GameObject WinScreen;
+
+    public GameObject Objective;
+
+    public GameObject Spawner;
+    public GameObject Enemy;
+
+    public GameObject InstrText;
+
+    private int Life;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        Life = gameObject.GetComponent<Objective>().objectiveHealth;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Life <= 0)
+        {
+            Lose();
+        }
+
+        //if ()
+        //{
+
+        //}
+
+
+
+
+	}
+
+    void Lose()
+    {
+        LoseScreen.SetActive(true);
+        Time.timeScale = 0f;
+        InstrText.SetActive(false);
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+
+    }
+
+    void Win()
+    {
+        WinScreen.SetActive(true);
+        Time.timeScale = 0f;
+        InstrText.SetActive(false);
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+    }
+
+    void Retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+}
